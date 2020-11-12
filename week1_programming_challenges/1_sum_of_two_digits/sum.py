786284561ebde48d03fae4b841d734ffd6b165a9
veldakarimi <veldakarimi@gmail.com>
2020-11-12 19:35:56 +0300

    file sections

--- week1_programming_challenges/1_sum_of_two_digits/sum.py
+++
@@ -1,9 +0,0 @@
-
-def sum_of_two_numbers(number_1, number_2):
-    return number_1 + number_2
-
-#print(sum(5,6))
-
-if __name__ == '____':
-    a, b= map(int, input().split())
-    print(sum(a, b))
